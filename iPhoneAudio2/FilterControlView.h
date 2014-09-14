9c8c1aa226184cb15869e3e10805fb28dc5207da
Chris Rivers <chris.rivers37@gmail.com>
2014-09-15 00:39:03 +0100

    Added Filter and skinned knobs

---
+++ iPhoneAudio2/FilterControlView.h
@@ -0,0 +1,13 @@
+//
+//  FilterControlView.h
+//  iPhoneAudio2
+//
+//  Created by Chris on 14/09/2014.
+//  Copyright (c) 2014 ccr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterControlView : UIView
+
+@end
---
+++ iPhoneAudio2/FilterControlView.m
@@ -0,0 +1,31 @@
+//
+//  FilterControlView.m
+//  iPhoneAudio2
+//
+//  Created by Chris on 14/09/2014.
+//  Copyright (c) 2014 ccr. All rights reserved.
+//
+
+#import "FilterControlView.h"
+
+@implementation FilterControlView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
