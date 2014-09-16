388e15ce4f902065a3a9adcb36423a4265d416f6
Chris Rivers <chris.rivers37@gmail.com>
2014-09-16 08:13:51 +0100

    Failed at filter envelope

---
+++ iPhoneAudio2/Envelope.h
@@ -0,0 +1,13 @@
+//
+//  Envelope.h
+//  iPhoneAudio2
+//
+//  Created by Chris on 15/09/2014.
+//  Copyright (c) 2014 ccr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Envelope : UIView
+
+@end
