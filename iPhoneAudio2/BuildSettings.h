187c6927f8bcd3bbfa44d5661d06a24c96cec980
Chris Rivers <chris.rivers37@gmail.com>
2014-09-30 20:26:50 +0100

    Renamed to Ogre

--- iPhoneAudio2/BuildSettings.h
+++
@@ -1,8 +0,0 @@
-
-#define IS_IPAD() (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-#define USE_ANALOG 0
-#define ANALOG_HARMONICS 15
-#define ROTARY_CONTROLS 0
-#define CV_FREQUENCY_RANGE 5000
-#define USER_DEFAULTS_PRESETS_KEY @"Presets"
-#define SCREEN_SCALE [[UIScreen mainScreen] scale]
